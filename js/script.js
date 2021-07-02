83c0e139775668de7c2833ec69fa60886192190d
MattiaR <rovitomattia91@gmail.com>
2021-07-02 13:49:57 +0200

    add esercizio+bonus

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
-// Descrizione:
-// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
-// Bonus Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+//! Descrizione:
+//! Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+//! Bonus Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
 
 const app = new Vue ({
 
-    el : 'app',
+    el : '#app',
 
     data :
     {
-
+        email : []
     },
     
-    
+    mounted(){
+        for (let i = 0; i < 10; i++){
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(element =>{
+                    console.log(element.data.response);
+                    this.email.push(element.data.response);
+                    console.log(this.email);
+                })
+        }
+        
+    }
 
 
 })
